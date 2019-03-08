b4102de21e8dc38b28e6366f4f36181012ed7b11
Longwelwind <longwelwind@gmail.com>
2019-03-08 23:55:31 +0100

    Add a space in the footer

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -38,8 +38,8 @@ export default class App extends Component<AppProps> {
 				</Grid>
 				<div style={{flexGrow: 1}}/>
 				<div className="footer">
-					<a target="_blank" href="https://github.com/Longwelwind/majority">Open-source</a> reimplementation by
-					<a target="_blank" href="https://longwelwind.net/">Longwelwind</a> of the game
+					<a target="_blank" href="https://github.com/Longwelwind/majority">Open-source</a> reimplementation
+					by <a target="_blank" href="https://longwelwind.net/">Longwelwind</a> of the game
 					of the same name from <a target="_blank" href="https://motion-twin.com/">
 					Motion Twin</a>.
 				</div>
